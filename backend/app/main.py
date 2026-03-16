a531a0e68f90e9b10130e084701871bb790843b2
SmeagolDanger <stormymichael@gmail.com>
2026-03-16 06:17:21 -0300

    123

--- backend/app/main.py
+++ backend/app/main.py
@@ -76,7 +76,8 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
         if settings.is_production:
             response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
         # Strip server header
-        response.headers.pop("server", None)
+        if "server" in response.headers:
+            del response.headers["server"]
         return response
 
 
